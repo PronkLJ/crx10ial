1e652f86d7c0bc41c27398e9cf323b4199bf3b9c
Lucas Pronk <l.j.pronk@utwente.nl>
2024-09-17 13:49:46 +0000

    fixed demo.launch.py

--- robot_moveit_config/launch/demo.launch.py
+++ robot_moveit_config/launch/demo.launch.py
@@ -6,6 +6,7 @@ def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("crx10ial", package_name="robot_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
 
+"""
 Includes
      * static_virtual_joint_tfs
      * robot_state_publisher
@@ -13,3 +14,4 @@ Includes
      * moveit_rviz
      * warehouse_db (optional)
      * ros2_control_node + controller spawners
+"""
