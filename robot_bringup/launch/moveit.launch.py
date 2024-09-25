09562f5d6ad5110632c837de06358bfc6b7357ba
Lucas Pronk <l.j.pronk@utwente.nl>
2024-09-25 11:07:18 +0000

    Gazebo + MoveIt combination is working

--- robot_bringup/launch/moveit.launch.py
+++ robot_bringup/launch/moveit.launch.py
@@ -1,58 +1,32 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import RegisterEventHandler
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launch_utils import DeclareBooleanLaunchArg
-import xacro
-
-# NOT FUNCTIONING
 
 def generate_launch_description():
-    
-    # Robot model
-    xacro_file = os.path.join(get_package_share_directory("robot_moveit_config"), "config", "crx10ial.urdf.urdf.xacro")
-    # Process xacro 
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
 
-    # Load the SRDF file
-    srdf_file = os.path.join(get_package_share_directory('robot_moveit_config'), 'config', 'crx10ial.srdf')
-    with open(srdf_file, 'r') as f:
-        srdf_content = f.read()
-
-    
-    # Configurations
-    ## Initialize MoveItConfigsBuilder to load robot description
-    moveit_config = MoveItConfigsBuilder(robot_name="crx10ial", package_name="robot_moveit_config").to_moveit_configs()
-    ## Rviz configuration file for MoveIt
-    rviz_config_file = os.path.join(get_package_share_directory("robot_moveit_config"), "config", "moveit.rviz")
-
-    # Nodes
-    ## Control Node
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[
-            moveit_config.robot_description,
-            os.path.join(get_package_share_directory("robot_moveit_config"), "config", "ros2_controllers.yaml")
-        ],
+    # Planning context
+    moveit_config=(
+        MoveItConfigsBuilder("robot")
+        .robot_description(os.path.join(get_package_share_directory('robot_description'), 'urdf', 'robot.xacro'))
+        .trajectory_execution(os.path.join(get_package_share_directory('robot_control'), 'config', 'moveit_controllers.yaml'))
+        .robot_description_kinematics(os.path.join(get_package_share_directory('robot_control'), 'config', 'kinematics.yaml'))
+        .joint_limits(os.path.join(get_package_share_directory('robot_control'), 'config', 'joint_limits.yaml'))
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .to_moveit_configs()
     )
-    # Robot State Publisher Node
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name="robot_state_publisher",
+    
+    # Start the actual move group node
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
         output="screen",
-        parameters=[{'robot_description': doc.toxml(),
-                     'robot_description_semantic': srdf_content,  # Ensure SRDF is passed here as well
-                     'use_sim_time': True,
-                     }],
+        parameters=[moveit_config.to_dict()]
     )
 
     ## RViz Node
@@ -60,57 +34,68 @@ def generate_launch_description():
         package="rviz2",
         executable="rviz2",
         name="rviz2",
-        respawn=False,
         output="log",
-        arguments=["-d", rviz_config_file],
+        arguments=["-d", os.path.join(get_package_share_directory('robot_control'), 'config', 'moveit.rviz')],
         parameters=[
-            moveit_config.planning_pipelines, 
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
             moveit_config.robot_description_kinematics,
+        ],
+    )
+
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"]
+    )
+
+    # Robot State Publisher Node
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name="robot_state_publisher",
+        output="both",
+        parameters=[moveit_config.robot_description],
+    )
+
+    ros2_controllers_path = os.path.join(get_package_share_directory("robot_control"), "config", "ros2_controllers.yaml")
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[moveit_config.robot_description, ros2_controllers_path],
+        output="both",
+    )
+
+    joint_state_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
+    )
+
+    manipulator_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["manipulator_controller"],
+        output="screen",
+    )
+
+    return LaunchDescription(
+        [
+            rviz_node,
+            static_tf,
+            robot_state_publisher,
+            ros2_control_node,
+            joint_state_controller,
+            manipulator_controller,
+
+            RegisterEventHandler(
+                OnProcessExit(
+                    target_action=joint_state_controller,
+                    on_exit=[move_group_node]
+                )
+            )
         ]
     )
-
-    # Launch
-    ## Virtual joints
-    virtual_joints_launch = os.path.join(get_package_share_directory("robot_moveit_config"), "launch", "static_virtual_joint_tfs.launch.py")
-    virtual_joints = None
-    if os.path.exists(virtual_joints_launch):
-        virtual_joints = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                str(virtual_joints_launch))
-    )
-
-    ## Move group
-    move_group = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory("robot_moveit_config"), "launch", "move_group.launch.py"))
-    )
-
-    # Load the joint state controller
-    load_joint_state_controller = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_state_broadcaster'],
-        output='screen',
-    )
-
-    # Load the manipulator controller
-    load_manipulator_controller = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['manipulator_controller'],
-        output='screen',
-    )
-            
-    return LaunchDescription([
-
-        # Nodes
-        control_node,
-        robot_state_publisher,
-        rviz_node,
-
-        # Launch
-        virtual_joints,
-        move_group,
-        load_joint_state_controller,
-        load_manipulator_controller,
-    ])
--- robot_bringup/launch/simulation.launch.py
+++ robot_bringup/launch/simulation.launch.py
@@ -1,13 +1,14 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch.event_handlers import OnShutdown
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import xacro
+
+from moveit_configs_utils import MoveItConfigsBuilder
 
 def generate_launch_description():
 
@@ -32,7 +33,7 @@ def generate_launch_description():
         name="robot_state_publisher",
         output="screen",
         parameters=[{'robot_description': doc.toxml(),
-                     #'use_sim_time': True,
+                     'use_sim_time': True,
                      }],
     )
 
@@ -59,6 +60,54 @@ def generate_launch_description():
         arguments=['manipulator_controller'],
         output='screen',
     )
+    
+    # planning context
+    moveit_config=(
+        MoveItConfigsBuilder("robot")
+        .robot_description(xacro_file)
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .robot_description_kinematics(file_path="config/kinematics.yaml")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .to_moveit_configs()
+    )
+    
+    # Start the actual move group node
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"moveit_simple_controller_manager": 
+             os.path.join(get_package_share_directory('robot_moveit_config'),'config','controllers.yaml')},
+            {'use_sim_time': True},        
+        ],
+    )
+
+    ## RViz Node
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", os.path.join(get_package_share_directory('robot_moveit_config'), 'config', 'moveit.rviz')],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {'use_sim_time': True} 
+        ],
+    )
+
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"]
+    )
 
     return LaunchDescription([
         gazebo,
@@ -66,4 +115,7 @@ def generate_launch_description():
         spawn_entity,
         load_joint_state_controller,
         load_manipulator_controller,
+        rviz_node,
+        static_tf,
+        move_group_node,
     ])
