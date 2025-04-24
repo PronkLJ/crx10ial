1029534cd7cd1c4841e81b1a7ce766fc43110a25
Lucas Pronk <l.j.pronk@utwente.nl>
2025-04-24 13:01:23 +0200

    repositioned ground plane

--- robot_motion_planning/src/add_ground_plane.cpp
+++ robot_motion_planning/src/add_ground_plane.cpp
@@ -14,7 +14,7 @@ public:
         // Define a collision object for the ground plane
         moveit_msgs::msg::CollisionObject ground_plane;
         ground_plane.id = "ground_plane";
-        ground_plane.header.frame_id = "base_link";
+        ground_plane.header.frame_id = "world";
 
         // Define a box (ground plane)
         shape_msgs::msg::SolidPrimitive primitive;
